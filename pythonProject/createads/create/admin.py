a0142770d4e399038f1b4bf596893acf365d5bc4
Asim Rimal <133133870+Asim-XXIV@users.noreply.github.com>
2024-06-28 18:01:29 +0545

    Delete pythonProject/createads directory

--- pythonProject/createads/create/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Advertisement, Category, UserWallet,UserTransaction
-
-admin.site.register(Advertisement)
-admin.site.register(Category)
-admin.site.register(UserWallet)
-admin.site.register(UserTransaction)
-
